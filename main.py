0761705d709319846f2ec9db3570f5769f82c7ed
Dar1an4 <ilepa.od@gmail.com>
2022-10-20 20:19:09 +0300

    add third func

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ print(random_name)
 
 random_male_name = names.get_full_name(gender='male')
 print(random_male_name)
+
+random_female_name = names.get_full_name(gender='female')
+print(random_female_name)
+
