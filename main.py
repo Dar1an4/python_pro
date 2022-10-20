f6fe4069fc2048b2f9abdff00b742f908504517d
Dar1an4 <ilepa.od@gmail.com>
2022-10-20 20:18:19 +0300

    add second func

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import names
 
 
-random_name1 = names.get_full_name()
-print(random_name1)
+random_name = names.get_full_name()
+print(random_name)
 
+random_male_name = names.get_full_name(gender='male')
+print(random_male_name)
